e113bbd8fab6f35618cd90db6e051f9226620e85
derektslim <derektslim@hotmail.com>
2025-03-26 15:40:17 +0800

    remove workspace from endpoints
    workspace no longer needed from endpoints

--- src/anaplanConnector/endpoints.py
+++ src/anaplanConnector/endpoints.py
@@ -1,44 +1,42 @@
 class Endpoints:
     def __init__(self,workspaceId=None,modelId=None):
-        self.workspaceId = workspaceId
         self.modelId = modelId
         self.fileId = None
         self.auth = 'https://auth.anaplan.com'
         self.api = 'https://api.anaplan.com/2/0'
         self.token = f'{self.auth}/token/authenticate'
-        self.workspaces = f'{self.api}/workspaces'
 
     def models(self):
-        return f'{self.api}/workspaces/{self.workspaceId}/models'
+        return f'{self.api}/models'
     
     def files(self):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/files'
+        return f'{self.api}/models/{self.modelId}/files'
 
     def file(self):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/files/{self.fileId}'
+        return f'{self.api}/models/{self.modelId}/files/{self.fileId}'
 
     def processes(self):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/processes'
+        return f'{self.api}/models/{self.modelId}/processes'
 
     def runProcess(self, processId):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/processes/{processId}/tasks'
+        return f'{self.api}/models/{self.modelId}/processes/{processId}/tasks'
 
     def processStatus(self, processId, taskId):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/processes/{processId}/tasks/{taskId}'
+        return f'{self.api}/models/{self.modelId}/processes/{processId}/tasks/{taskId}'
 
     def chunk(self, fileId, chunkNum):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/files/{fileId}/chunks/{chunkNum}'
+        return f'{self.api}/models/{self.modelId}/files/{fileId}/chunks/{chunkNum}'
 
     def exports(self):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/exports'
+        return f'{self.api}/models/{self.modelId}/exports'
 
     def startExport(self,exportId):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/exports/{exportId}/tasks'
+        return f'{self.api}/models/{self.modelId}/exports/{exportId}/tasks'
 
     def taskStatus(self, exportId, taskId):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/exports/{exportId}/tasks/{taskId}'
+        return f'{self.api}/models/{self.modelId}/exports/{exportId}/tasks/{taskId}'
 
     def getNumChunks(self, fileId):
-        return f'{self.api}/workspaces/{self.workspaceId}/models/{self.modelId}/files/{fileId}/chunks'
+        return f'{self.api}/models/{self.modelId}/files/{fileId}/chunks'
 
         
